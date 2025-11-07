6b44fe98ee9ed683dabb443244872efb13f8c717
Aleksey Komissarov <aleksey.komissarov@saritasa.com>
2025-11-07 18:04:20 +0500

    Added boosts initialization, improved app ui

--- CSharpClicker/Intitialization/DbContextInitializer.cs
+++ CSharpClicker/Intitialization/DbContextInitializer.cs
@@ -1,4 +1,5 @@
-﻿using CSharpClicker.Infrastructure.Implementations;
+﻿using CSharpClicker.Domain;
+using CSharpClicker.Infrastructure.Implementations;
 using Microsoft.EntityFrameworkCore;
 using SQLitePCL;
 
@@ -16,6 +17,87 @@ public static class DbContextInitializer
     public static void InitializeDataBase(AppDbContext dbContext)
     {
         dbContext.Database.Migrate();
+        AddBoostsIfNotExist(dbContext);
+    }
+
+    private static void AddBoostsIfNotExist(AppDbContext dbContext)
+    {
+        var boosts = new Boost[]
+        {
+            new()
+            {
+                Title = "Стальная кирка",
+                Price = 10,
+                Profit = 1,
+                IsAuto = false,
+                Image = GetImageBytes("pickaxe.png"),
+            },
+            new()
+            {
+                Title = "Крестьянин",
+                Price = 50,
+                Profit = 1,
+                IsAuto = true,
+                Image = GetImageBytes("peasant.png"),
+            },
+            new()
+            {
+                Title = "Шахта",
+                Price = 100,
+                Profit = 10,
+                IsAuto = false,
+                Image = GetImageBytes("shaft.png"),
+            },
+            new()
+            {
+                Title = "Священник",
+                Price = 500,
+                Profit = 10,
+                IsAuto = true,
+                Image = GetImageBytes("priest.png"),
+            },
+            new()
+            {
+                Title = "Динамит",
+                Price = 1000,
+                Profit = 100,
+                IsAuto = false,
+                Image = GetImageBytes("explosion.png"),
+            },
+            new()
+            {
+                Title = "Слоник",
+                Price = 5000,
+                Profit = 100,
+                IsAuto = true,
+                Image = GetImageBytes("elephant.png"),
+            },
+            new()
+            {
+                Title = "Химическая обработка",
+                Price = 10000,
+                Profit = 1000,
+                IsAuto = false,
+                Image = GetImageBytes("chemistry.png"),
+            },
+        };
+
+        foreach (var boost in boosts)
+        {
+            var exists = dbContext.Boosts.Any(b => b.Title == boost.Title);
+            if (!exists)
+            {
+                dbContext.Boosts.Add(boost);
+            }
+        }
+
+        dbContext.SaveChanges();
+
+        static byte[] GetImageBytes(string imageName)
+        {
+            var filePath = Path.Combine(AppContext.BaseDirectory, "resources", imageName);
+            return File.ReadAllBytes(filePath);
+        }
     }
 
     public static string GetPathToDatabaseFile()
